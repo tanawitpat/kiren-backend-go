453fe6c30e709220b93c81100d3c5e7a1181c98f
Tanawit Pattanaveerangkoon <tanawit.pat@gmail.com>
2019-09-13 20:00:20 +0700

    Add category_id and image_path to product struct

--- internal/pkg/product/model.go
+++ internal/pkg/product/model.go
@@ -6,12 +6,15 @@ import (
 
 // Product represents the product model
 type Product struct {
-	ID            string  `json:"product_id"`
-	NameTH        string  `json:"product_name_th"`
-	NameEN        string  `json:"product_name_en"`
-	DescriptionTH string  `json:"product_description_th"`
-	DescriptionEN string  `json:"product_description_en"`
-	Price         float64 `json:"product_price"`
+	ID               string  `json:"product_id"`
+	NameTH           string  `json:"product_name_th"`
+	NameEN           string  `json:"product_name_en"`
+	DescriptionTH    string  `json:"product_description_th"`
+	DescriptionEN    string  `json:"product_description_en"`
+	Price            float64 `json:"product_price"`
+	ProductImagePath string  `json:"product_image_path"`
+	BestSellerFlag   bool    `json:"best_seller_flag"`
+	CategoryID       string  `json:"category_id"`
 }
 
 // GetProductsResponse represents the response model of GetProducts endpoint.
