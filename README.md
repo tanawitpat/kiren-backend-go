ea05592b03de200ad9e9cfb3c181adccacf7a83a
Tanawit Pattanaveerangkoon <tanawit.pat@gmail.com>
2019-09-14 23:47:41 +0700

    Add k8s deployment files

---
+++ README.md
@@ -0,0 +1,22 @@
+# kiren-backend-go
+
+**URL:** <https://kiren.tanawitp.me>
+
+## Commands
+
+```bash
+# Install libraries
+make install
+
+# Build docker image
+make build
+
+# Run the service
+make run
+
+# Run unit tests
+make unit_test
+
+# Deploy
+make deploy GCLOUD_PROJECT_ID=<GCLOUD_PROJECT_ID>
+```
--- internal/app/logger.go
+++ internal/app/logger.go
@@ -6,12 +6,12 @@ import (
 
 var log = logrus.New()
 
-const env = "dev"
+const env = "local"
 
 // InitLogger initializes a logger with timestamp field.
 func InitLogger() *logrus.Logger {
 	switch env {
-	case "dev":
+	case "local":
 		log.Formatter = &logrus.TextFormatter{
 			TimestampFormat: "2006-01-02 15:04:05",
 			FullTimestamp:   true,
