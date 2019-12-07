1f2bb68e2c65f3c550c0b26cd44ee36b531a2bc7
Tanawit Pattanaveerangkoon <tanawit.pat@gmail.com>
2019-12-07 13:21:38 +0700

    Leverage the AWS Lambda Proxy Request functionality

--- lambda/ping/main.go
+++ lambda/ping/main.go
@@ -3,11 +3,19 @@ package main
 import (
 	"kiren-backend-go/internal/core/pingpong"
 
+	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
-func handler() (string, error) {
-	response, _ := pingpong.Ping()
+func handler() (events.APIGatewayProxyResponse, error) {
+	body, statusCode := pingpong.Ping()
+	response := events.APIGatewayProxyResponse{
+		Body:       body,
+		StatusCode: statusCode,
+		Headers: map[string]string{
+			"Content-Type": "text/plain",
+		},
+	}
 	return response, nil
 }
 
--- lambda/products/main.go
+++ lambda/products/main.go
@@ -1,22 +1,40 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"kiren-backend-go/internal/app"
 	"kiren-backend-go/internal/core/product"
 
+	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
-func handler() (product.GetProductsResponse, error) {
-	logger := app.InitLogger()
+func handler() (events.APIGatewayProxyResponse, error) {
+	var buf bytes.Buffer
 
+	logger := app.InitLogger()
 	if err := app.InitConfigEnv(); err != nil {
-		logger.Printf("Init config error: %+v", err)
+		logger.Infof("Init config error: %+v", err)
 		panic(err)
 	}
-	logger.Printf("Initial app config: %+v", app.CFG)
+	logger.Infof("Initial app config: %+v", app.CFG)
 
-	response, _ := product.GetProducts()
+	logicResponse, statusCode := product.GetProducts()
+	body, err := json.Marshal(logicResponse)
+	if err != nil {
+		logger.Errorf("Cannot convert logic response to []byte: %+v", err)
+		return events.APIGatewayProxyResponse{StatusCode: 500}, err
+	}
+	json.HTMLEscape(&buf, body)
+
+	response := events.APIGatewayProxyResponse{
+		Body:       buf.String(),
+		StatusCode: statusCode,
+		Headers: map[string]string{
+			"Content-Type": "application/json",
+		},
+	}
 	return response, nil
 }
 
