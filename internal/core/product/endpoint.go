62f9ddf6274dd92ec0c1aa53740e4f6ece8d6152
Tanawit Pattanaveerangkoon <tanawit.pat@gmail.com>
2019-12-08 21:23:15 +0700

    Create the best seller products endpoint and its serverless function

--- internal/core/product/endpoint.go
+++ internal/core/product/endpoint.go
@@ -15,6 +15,7 @@ func GetProducts() (GetProductsResponse, int) {
 	// Initialize response model
 	res := GetProductsResponse{}
 
+	// Query products data
 	selDB, err := app.Query(`
 		SELECT product_id, product_name_th, product_description_th, product_image_path, product_price 
 		FROM products 
@@ -28,6 +29,7 @@ func GetProducts() (GetProductsResponse, int) {
 		return res, app.ERR.InternalServerError.Code
 	}
 
+	// Map the queried data with the products model
 	products := Products{}
 	for selDB.Next() {
 		var productID, productNameTH, productNameEN, productDescriptionTH, productDescriptionEN, productImagePath sql.NullString
@@ -55,9 +57,9 @@ func GetProducts() (GetProductsResponse, int) {
 		product := MapSQLNullToProduct(productSQLNull)
 		products = append(products, product)
 	}
-
-	logger.Debugf("Product data: %+v", products)
 	res.ProductData = products
+	logger.Debugf("Product data: %+v", products)
+
 	return res, http.StatusOK
 }
 
@@ -67,10 +69,12 @@ func GetProduct(productID string) (GetProductResponse, int) {
 	// Initialize logger
 	logger := app.InitLogger()
 
-	// Initialize response model
+	// Initialize response models
 	res := GetProductResponse{}
 	product := Product{}
+	relevantProducts := Products{}
 
+	// Return error if no product ID provided
 	if productID == "" {
 		logger.Errorf("%s", "No productID provided")
 		res.Error = app.ErrorResp{
@@ -79,6 +83,8 @@ func GetProduct(productID string) (GetProductResponse, int) {
 		}
 		return res, app.ERR.BadRequest.Code
 	}
+
+	// Query product data using product ID and map the data with the product model
 	sqlRow, err := app.QueryRow(`
 		SELECT product_id, product_name_th, product_description_th, product_image_path, product_price 
 		FROM products 
@@ -93,9 +99,53 @@ func GetProduct(productID string) (GetProductResponse, int) {
 		return res, app.ERR.InternalServerError.Code
 	}
 	sqlRow.Scan(&product.ID, &product.NameTH, &product.DescriptionTH, &product.ProductImagePath, &product.Price)
-
-	logger.Debugf("Product data: %+v", product)
 	res.ProductData = product
+
+	// Select products randomly for product recommendation
+	sqlRowRelevantProducts, err := app.Query(`
+		SELECT product_id, product_name_th, product_description_th, product_image_path, product_price
+		FROM products
+		ORDER BY RAND()
+		LIMIT 4
+	`)
+	if err != nil {
+		logger.Errorf("%s: %+v", "Cannot fetch relevant product data", err)
+		res.Error = app.ErrorResp{
+			Name:    app.ERR.InternalServerError.Name,
+			Message: app.ERR.InternalServerError.Message,
+		}
+		return res, app.ERR.InternalServerError.Code
+	}
+
+	// Map the queried data with the products struct
+	for sqlRowRelevantProducts.Next() {
+		var productID, productNameTH, productNameEN, productDescriptionTH, productDescriptionEN, productImagePath sql.NullString
+		var productPrice sql.NullFloat64
+		var productBestSellerFlag sql.NullBool
+		err = sqlRowRelevantProducts.Scan(&productID, &productNameTH, &productDescriptionTH, &productImagePath, &productPrice)
+		if err != nil {
+			logger.Errorf("%s: %+v", "Cannot fetch products data", err)
+			res.Error = app.ErrorResp{
+				Name:    app.ERR.InternalServerError.Name,
+				Message: app.ERR.InternalServerError.Message,
+			}
+			return res, app.ERR.InternalServerError.Code
+		}
+		productSQLNull := ProductSQLNull{
+			ID:               productID,
+			NameTH:           productNameTH,
+			NameEN:           productNameEN,
+			DescriptionTH:    productDescriptionTH,
+			DescriptionEN:    productDescriptionEN,
+			ProductImagePath: productImagePath,
+			Price:            productPrice,
+			BestSellerFlag:   productBestSellerFlag,
+		}
+		relevantProduct := MapSQLNullToProduct(productSQLNull)
+		relevantProducts = append(relevantProducts, relevantProduct)
+	}
+	res.RelevantProductData = relevantProducts
+	logger.Debugf("Relavant product data: %+v", relevantProducts)
 
 	return res, http.StatusOK
 }
@@ -109,10 +159,14 @@ func GetBestSellerProducts() (BestSellerProductResponse, int) {
 	// Initialize response model
 	res := BestSellerProductResponse{}
 
-	// Load all products
-	products, err := loadProduct()
+	// Query products data
+	selDB, err := app.Query(`
+		SELECT product_id, product_name_th, product_description_th, product_image_path, product_price 
+		FROM products
+		WHERE best_seller_flag="Y"
+	`)
 	if err != nil {
-		logger.Errorf("%s: %+v", "Cannot fetch product data", err)
+		logger.Errorf("%s: %+v", "Query failed", err)
 		res.Error = app.ErrorResp{
 			Name:    app.ERR.InternalServerError.Name,
 			Message: app.ERR.InternalServerError.Message,
@@ -120,17 +174,36 @@ func GetBestSellerProducts() (BestSellerProductResponse, int) {
 		return res, app.ERR.InternalServerError.Code
 	}
 
-	// Filter best seller products using best_seller_flag
-	bestSellerProducts := products.getBestSellerProduct()
-	if err != nil {
-		logger.Errorf("%s: %+v", "Cannot select product data", err)
-		res.Error = app.ErrorResp{
-			Name:    app.ERR.InternalServerError.Name,
-			Message: app.ERR.InternalServerError.Message,
+	// Map the queried data with the products model
+	products := Products{}
+	for selDB.Next() {
+		var productID, productNameTH, productNameEN, productDescriptionTH, productDescriptionEN, productImagePath sql.NullString
+		var productPrice sql.NullFloat64
+		var productBestSellerFlag sql.NullBool
+		err = selDB.Scan(&productID, &productNameTH, &productDescriptionTH, &productImagePath, &productPrice)
+		if err != nil {
+			logger.Errorf("%s: %+v", "Cannot fetch products data", err)
+			res.Error = app.ErrorResp{
+				Name:    app.ERR.InternalServerError.Name,
+				Message: app.ERR.InternalServerError.Message,
+			}
+			return res, app.ERR.InternalServerError.Code
 		}
-		return res, app.ERR.InternalServerError.Code
+		productSQLNull := ProductSQLNull{
+			ID:               productID,
+			NameTH:           productNameTH,
+			NameEN:           productNameEN,
+			DescriptionTH:    productDescriptionTH,
+			DescriptionEN:    productDescriptionEN,
+			ProductImagePath: productImagePath,
+			Price:            productPrice,
+			BestSellerFlag:   productBestSellerFlag,
+		}
+		product := MapSQLNullToProduct(productSQLNull)
+		products = append(products, product)
 	}
+	res.ProductData = products
+	logger.Debugf("Best seller product data: %+v", products)
 
-	res.ProductData = bestSellerProducts
 	return res, http.StatusOK
 }
